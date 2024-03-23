892afa6193df2fe5dad0da9bb9e8e7e118864000
Justin Hehli <justinsimon.hehli@uzh.ch>
2024-03-23 13:56:59 +0100

    saveDataNode: refactor, add automatic meta.json creation

--- frankaRobot/saveDataNode.py
+++ frankaRobot/saveDataNode.py
@@ -8,6 +8,7 @@ Files Created:
 1. all_data.txt: Contains all data published by the Franka Panda robot.
 2. true_label.csv: Contains data of true labels acquired through CANBUS communication.
 3. model_result.csv: Presents the model output and data window.
+4. meta.json: meta data about the run / saved data
 
 How to Run:
 1. Unlock the Robot:
@@ -33,7 +34,7 @@ How to Run:
         /home/mindlab/miniconda3/envs/frankapyenv/bin/python3 /home/mindlab/humanObjectDetection/frankaRobot/saveDataNode.py
 """
 
-## Import required libraries 
+# Import required libraries
 import numpy as np
 import rospy
 from std_msgs.msg import Float64
@@ -41,57 +42,83 @@ from rospy_tutorials.msg import Floats
 from rospy.numpy_msg import numpy_msg
 from franka_interface_msgs.msg import RobotState
 import csv
-import datetime
 import os
+import json
+import git
+from pathlib import Path
 
-# Set the default PATH
-PATH = '/home/mindlab/robotHumanObjectDetectionDataset/20240316/'
 
-# Prompt the user to enter a tag name for data labeling
-folder_name = input('Enter tag name: ')
+contact_type_shorts = {"s": "soft", "h": "hard"}
+
+# Set base path for saving data as <repository-root>/dataset/
+git_repo = git.Repo(os.getcwd(), search_parent_directories=True)
+ROOT_PATH = Path(git_repo.git.rev_parse("--show-toplevel")) / "dataset"
+
+# Prompt the user to enter a tag name and contact type
+FOLDER_TAG = input('Enter tag name: ')
+
+contact_type = input("Enter contact type (soft: s / hard: h): ")
+contact_type = contact_type_shorts[contact_type.lower()]
+
 
 class LogData:
-    def __init__(self, PATH: str) -> None:
+    def __init__(self) -> None:
         # Initialize ROS node
         rospy.init_node('log_data')
         print('ROS node is initiated!')
-        
+
         # Create a folder for saving data
-        self.PATH = PATH + folder_name
-        os.mkdir(self.PATH)
-        print("Directory '%s' created" % self.PATH)
+        self.PATH = ROOT_PATH / FOLDER_TAG
+        os.mkdir(str(self.PATH.absolute()))
 
         # Create empty files for saving data
-        self.file_all = open(self.PATH + '/all_data.txt', 'w')
-        self.file_index = csv.writer(open(self.PATH + '/true_label.csv', 'w'))
-        self.file_index.writerow(('time_sec', 'time_nsec', 'timestamp', 'DATA0', 'DATA1', 'DATA2', 'DATA3', 'DATA4'))
+        all_data_path = str((self.PATH / 'all_data.txt').absolute())
+        true_label_path = str((self.PATH / 'true_label.csv').absolute())
+        model_results_path = str((self.PATH / 'model_result.csv').absolute())
+        meta_path = str((self.PATH / 'meta.json').absolute())
 
-        self.log_model_result = csv.writer(open(self.PATH + '/model_result.csv', 'w'))
-        self.log_model_result.writerow(('Time_sec', 'Time_nsec', 'prediction_duration', 'contact_out', 'collision_out', 'localization_out'))
-        print('*** Four empty text files are created in ', self.PATH, ' ***')
+        self.all_data_file = open(all_data_path, 'w')
+
+        self.true_label_file = csv.writer(open(true_label_path, 'w'))
+        self.true_label_file.writerow(
+            ('time_sec', 'time_nsec', 'timestamp', 'DATA0'))
+
+        self.model_results_file = csv.writer(open(model_results_path, 'w'))
+        self.model_results_file.writerow(
+            ('Time_sec', 'Time_nsec', 'prediction_duration', 'contact_out', 'collision_out', 'localization_out'))
+
+        meta_data = {"contact_type": contact_type, "start_from_time": -1}
+        with open(meta_path, 'w') as f:
+            json.dump(meta_data, f, indent=4)
+
+        print('*** Initialized files in ', str(self.PATH.absolute()), ' ***')
 
         # Subscribe to relevant ROS topics
-        rospy.Subscriber(name="/model_output", data_class=numpy_msg(Floats), callback=self.save_model_output)
-        rospy.Subscriber(name="/robot_state_publisher_node_1/robot_state", data_class=RobotState, callback=self.save_robot_state)
-        rospy.Subscriber(name="/contactTimeIndex", data_class=numpy_msg(Floats), callback=self.save_contact_index)
+        rospy.Subscriber(
+            name="/model_output", data_class=numpy_msg(Floats), callback=self.save_model_output)
+        rospy.Subscriber(name="/robot_state_publisher_node_1/robot_state",
+                         data_class=RobotState, callback=self.save_robot_state)
+        rospy.Subscriber(name="/contactTimeIndex",
+                         data_class=numpy_msg(Floats), callback=self.save_contact_index)
 
     def save_contact_index(self, data):
         # Save contact index data to true_label.csv
         data_row = np.array(data.data)
-        self.file_index.writerow(data_row)
+        self.true_label_file.writerow(data_row)
 
     def save_model_output(self, data):
         # Save model output data to model_result.csv
         data_row = np.array(data.data)
-        self.log_model_result.writerow(data_row)
+        self.model_results_file.writerow(data_row)
 
     def save_robot_state(self, data):
         # Save robot state data to all_data.txt
-        self.file_all.write(str(data))
+        self.all_data_file.write(str(data))
+
 
 if __name__ == "__main__":
     # Create an instance of the LogData class
-    log_data_instance = LogData(PATH)
-    
+    log_data_instance = LogData()
+
     # Keep the program running to listen for ROS messages
     rospy.spin()
